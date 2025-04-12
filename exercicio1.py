09fd32c2e1f59c88ab51f258b6d1521f7eaeaba2
brunoadsdados <brunosilvacorretorjf@gmail.com>
2025-04-12 18:06:23 -0300

    adicionei exercicio 2

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,24 @@
-print('Digite')
+#Expressões algébricas:
+#Escreva as seguintes expressões algébricas em linguagem Python:
+
+#a) O somatório dos 5 primeiros números inteiros e positivos:
+print(1 + 2 + 3 + 4 + 5)
+
+#b) A média entre 23, 19 e 31:
+print((23 + 19 + 31) /3)
+
+#c) O número de vezes que 73 cabe (ou é dividido) em 403:
+print(403 // 73) # '//' dá o número inteiro
+
+#d) A sobra quando 403 é dividido por 73:
+print(403 % 73)
+
+#e) 2 elevado a 10ª potência:
+print(2 ** 10)
+
+#f) O valor absoluto (sem sinal negativo) da diferença entre 54 e 57:
+print(abs(54 - 57)) # 'abs()' transforma em absoluto
+
+#g) O menor valor entre 34, 29 e 31:
+print(min(34, 29, 31)) # 'min()' pega o menor valor entre eles
+
---
+++ exercicio2.py
@@ -0,0 +1,11 @@
+#Escreva as expressões em Python para:
+
+# a) Atribuir o valor inteiro 3 À variável 'a':
+a = 3
+
+# b) Atribuir o valor 4 à variável 'b':
+b = 4
+
+# c) Atribuir À variável c o valor da expressão 'a * a + b * c':
+c = (a * b) + (b * b)
+print(c)
